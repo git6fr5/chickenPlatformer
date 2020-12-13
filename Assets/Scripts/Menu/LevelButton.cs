ee85e15c48dddb72c01d9a738e9994be66cda7ff
gearless_june <taha.akbarally@gmail.com>
2020-12-13 08:31:44 +0530

    created menu system

---
+++ Assets/Scripts/Menu/LevelButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
